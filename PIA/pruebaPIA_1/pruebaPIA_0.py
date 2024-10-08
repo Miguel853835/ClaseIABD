48f569274196fc02ab3021a6802ca7c62c55eb54
BigChungus <alexcruzz267@gmail.com>
2024-10-08 17:24:17 +0200

    Pruebas con carlos 08/10/2024

--- PIA/pruebaPIA_1/pruebaPIA_0.py
+++ PIA/pruebaPIA_1/pruebaPIA_0.py
@@ -1,19 +1,26 @@
-import turtle
-tt=turtle.Turtle()
-turtle.bgcolor("cyan")
-tt.pencolor("darkblue")
-tt.speed(0)
-tt.penup()
-tt.goto(0,200)
-tt.pendown()
-forDis=0
-dR=0
-while(True):
-    tt.forward(forDis)
-    tt.right(dR)
-    forDis+=1.75
-    dR+=0.5
-    if dR==1000:
-        break
-    tt.hideturtle()
-turtle.done
+# 1.- Ejercicio 1
+# Escrito mediante un print simple
+print('¡Hola Chell!')
+print('Aqui tienes tu "tarta".')
+print(80085)
+
+# 2.- Print con saltos de espacios
+print("\n¡Ja, \n \t ja, \n \t \t ja! \n")
+
+# 3.- Imprimimos el mensaje que pedirá la información al usuário en el input, y lo mostrará al usuario
+print("Tu hija se llama:", input("¿Cómo se llama tu hija?"), input("¿Y cómo se apellida?"))
+print("\n")
+
+# 4.- Usos de vairables
+coche = input("Nombre de tu coche favorito:")
+ciudad = input("Nombre de tu lugar favorito:")
+numero = input("Número favorito:")
+
+print("¡He ganado la loteria!\n¡Me han tocado " + numero + " millones de €!\nMe compraré un " + coche + " y una casa en " + ciudad + "\n")
+
+# 5.- Formatear cadenas
+juego = "La Ruleta Rusa"
+puntos_anteriores = 10
+puntos_nuevos = 7
+
+print(f"¡Has ganado {puntos_nuevos} puntos en el juego de {juego}! ¡Llevas acumulados {puntos_anteriores + puntos_nuevos}!")
